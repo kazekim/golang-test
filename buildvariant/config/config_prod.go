726769fa6d3672b21442232632b02d12765e46b2
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-09-08 01:36:38 +0700

    add build variant code

---
+++ buildvariant/config/config_prod.go
@@ -0,0 +1,15 @@
+// +build prod
+
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package config
+
+const (
+	MYSQL_HOST  = "127.0.0.1"
+	MYSQL_DATABASE = "kazekim_prod"
+	MYSQL_PORT = "3306"
+	MYSQL_USERNAME = "db_prod"
+	MYSQL_PASSWORD = "jirawat"
+)
---
+++ buildvariant/config/config_staging.go
@@ -0,0 +1,15 @@
+// +build staging
+
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package config
+
+const (
+	MYSQL_HOST  = "127.0.0.1"
+	MYSQL_DATABASE = "kazekim_staging"
+	MYSQL_PORT = "3306"
+	MYSQL_USERNAME = "db_staging"
+	MYSQL_PASSWORD = "jirawat"
+)
