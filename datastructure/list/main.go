be867d28093dc06a6748e4025032823fc60fb1f0
Jirawat Harnsiriwatanakit <jirawat.h@kazekim.com>
2019-12-26 23:39:42 +0700

    change package to main

--- datastructure/list/main.go
+++ datastructure/list/main.go
@@ -1,1 +1,39 @@
-package list
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+
+	val2 := 18
+	var intList list.List
+	intList.PushBack(7)
+	intList.PushBack(val2)
+	intList.PushBack(35)
+
+	printForward(intList)
+	printBackWard(intList)
+
+	elem := intList.Front()
+
+	intList.InsertAfter(4, elem)
+
+	printForward(intList)
+
+	fmt.Println("Len : ", intList.Len())
+
+}
+
+func printForward(valList list.List) {
+	for element := valList.Front(); element != nil; element=element.Next() {
+		fmt.Println(element.Value.(int))
+	}
+}
+
+func printBackWard(valList list.List) {
+	for element := valList.Back(); element != nil; element=element.Prev() {
+		fmt.Println(element.Value.(int))
+	}
+}
